4cf89d5e857be223b2df61dea32fe9c7cd439cc9
Adonis <adonislatham@microsoft.com>
2022-07-04 22:30:08 -0500

    Update function Auth

--- Source/Microsoft.Teams.Apps.CompanyCommunicator.SendWrapper.Func/Export/DataExportFunction.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator.SendWrapper.Func/Export/DataExportFunction.cs
@@ -43,7 +43,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.SendWrapper.Func.Export
 
         [FunctionName("DataExportFunction")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
