fd94ebf7a7ec84e51177da7642eb931bf1862346
Adonis <adonislatham@microsoft.com>
2022-07-08 18:20:15 -0500

    update

--- Deployment/scripts/create-rg-managed-identity.ps1
+++ Deployment/scripts/create-rg-managed-identity.ps1
@@ -8,14 +8,11 @@ Connect-MgGraph -AccessToken $token.Token
 $appregId = az identity create -g rg-company-communicator -n AppRegCreator --out tsv --query principalId
 
 $checkUri = "https://graph.microsoft.com/v1.0//roleManagement/directory/roleAssignments?$filter=principalId eq '$appregId'" 
-az rest --method get --uri $checkUri --headers Content-Type=application/json --query "[].{principalId:principalId}[?principalId=='$appredId']" -o table
-
 
 $results = az rest --method get --uri $checkUri --headers Content-Type=application/json --query value | convertfrom-json
 $exists = ($results | Where-Object {$_.principalId -eq "$appregId"}).principalId
 
-if(!$exists) {
-    $principalId = $appregId
+if($null -eq $exists) {
     $roleDefinitionId = '9b895d92-2cd3-44c7-9d02-a6ac2d5ea5c3'
     $directoryScopeId = "/"
     $body = "{'principalId':'$appregId','roleDefinitionId':'$roleDefinitionId','directoryScopeId':'$directoryScopeId'}"
